d8f7324d419e93b3ee640762ee0fa079bfb8477a
Ayush Patel <ayushpatel.2026@gmail.com>
2024-01-12 18:00:17 -0500

    version3

--- src/main/java/ca/mcscert/se2aa4/demos/tennis/Main.java
+++ src/main/java/ca/mcscert/se2aa4/demos/tennis/Main.java
@@ -8,9 +8,6 @@ import org.apache.commons.cli.ParseException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Main {
 
     private static final Logger logger = LogManager.getLogger(MyClass.class);
